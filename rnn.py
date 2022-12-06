11e427ffe841ad75aa6299c3aac9531875fc5ce0
hello3196 <45480548+hello3196@users.noreply.github.com>
2022-12-06 17:35:53 +0900

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -30,9 +30,8 @@ class Net(nn.Module):
         super(Net,self).__init__()
         self.lstm = nn.LSTM(3,32, batch_first=True, bidirectional=True)
         
-    def forward(self, x1, x2):
-        _, (h_n_1, _) = self.lstm(x1)
-        _, (h_n_2, _) = self.lstm(x2)
-        score = torch.bmm(h_n_1, h_n_2)
+    def forward(self, X, x5): # X: [batch_size, seq_length, 5], x5: [batch_size, 5]
+        _, (h_n_1, _) = self.lstm(X)
+        score = torch.bmm(h_n_1, x5)
         loss = -F.logsigmoid(score).squeeze()
         return loss.mean()
